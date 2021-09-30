50163b85edfdfb363c6a2f23e1583164d458113a
Jeremiah Ikwuje <ikwuje24@gmail.com>
2021-09-30 08:13:51 +0100

    got navbar working

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,24 +1,43 @@
 <script>
+	let isOpen = false;
 	export let segment;
+
+	function toggleNav() {
+		isOpen = !isOpen;
+	}
 </script>
 
-<nav class="navbar" role="navigation" aria-label="main-navigation">
+<style>
+	@media(min-width: 769px) {
+		.nav {
+			border-bottom: 1px solid gray;
+		}
+	}
+</style>
+
+<nav class="navbar nav" role="navigation" aria-label="main-navigation">
 	<div class="navbar-brand">
-		<a href="/" class="navbar-brand-item">
-			<img src="logo.png" height="38px" width="38px" alt="Covid 19 Logo">
+		<a href="/" class="navbar-item">
+			<img src="logo.png" height="38px" width="38px" alt="Covid 19">
 		</a>
 
-		<span class="navbar-burger" aria-label="menu" aria-expanded="false">
+		<span
+			class="navbar-burger"
+			aria-label="menu"
+			aria-expanded="false"
+			on:click={toggleNav}
+			class:is-active={isOpen}
+		>
 			<span aria-hidden="true"></span>
 			<span aria-hidden="true"></span>
 			<span aria-hidden="true"></span>
 		</span>
 	</div>
 
-	<div class="navbar-menu">
+	<div class="navbar-menu" class:is-active={isOpen}>
 		<div class="navbar-start">
-			<a class="navbar-item" href="/" aria-current="{segment}">Home</a>
-			<a class="navbar-item" href="/about">About</a>
+			<a class="navbar-item" href="/" class:is-active={segment === undefined}>Home</a>
+			<a class="navbar-item" href="/about" class:is-active={segment === 'about'}>About</a>
 		</div>
 	</div>
 </nav>
