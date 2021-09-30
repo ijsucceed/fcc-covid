a9f0fcb9e62bcc6c791ebc6fb17c6b2131d8b13e
Jeremiah Ikwuje <ikwuje24@gmail.com>
2021-09-30 07:44:51 +0100

    first commit

---
+++ src/components/Nav.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let segment;
+</script>
+
+<nav class="navbar" role="navigation" aria-label="main-navigation">
+	<div class="navbar-brand">
+		<a href="/" class="navbar-brand-item">
+			<img src="logo.png" height="38px" width="38px" alt="Covid 19 Logo">
+		</a>
+
+		<span class="navbar-burger" aria-label="menu" aria-expanded="false">
+			<span aria-hidden="true"></span>
+			<span aria-hidden="true"></span>
+			<span aria-hidden="true"></span>
+		</span>
+	</div>
+
+	<div class="navbar-menu">
+		<div class="navbar-start">
+			<a class="navbar-item" href="/" aria-current="{segment}">Home</a>
+			<a class="navbar-item" href="/about">About</a>
+		</div>
+	</div>
+</nav>
---
+++ src/routes/_error.svelte
@@ -0,0 +1,6 @@
+<script>
+	export let status;
+	export let error;
+</script>
+
+<span>{error} {status}</span>
